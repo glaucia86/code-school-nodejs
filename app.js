b814623b470b7819b84aa3e4d0e1dda4a4c26b97
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-15 21:28:55 -0300

    Alteração no arquivo app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-    response.writeHead(200);
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
     
     /* Função para poder realizar a leitura do arquivo 'index.html' */
     fs.readFile('index.html', function(error, contents) {
